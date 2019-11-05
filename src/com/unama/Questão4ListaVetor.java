11894a6d71fb2dff7f38e9ccc7f6dd16b98dc124
04052973 <andreyvictorfotografias@outlook.com>
2019-11-05 10:29:03 -0300

    hello

--- src/com/unama/Questão4ListaVetor.java
+++ src/com/unama/Questão4ListaVetor.java
@@ -1,4 +1,21 @@
 package com.unama;
 
+import java.util.Scanner;
+
 public class Questão4ListaVetor {
-}
+    public static void main(String[] args) {
+        //entrada de dados
+        Scanner scanner = new Scanner (System.in);
+        double[] vetor = new double[3];
+        for (int i=0; i<vetor.length; i++){
+            System.out.println("Informe o número: " +i);
+            vetor[i] = scanner.nextDouble();
+        }
+        //chamada da função
+        double[] resultado = verificaNumeroPosicao(vetor);
+        //saída de dados
+        for(double numero: resultado){
+            System.out.println(numero);
+        }
+        }
+    }
